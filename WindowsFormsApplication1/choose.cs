2a3d4bcc38ab214c68670248c61ec9a9632c470f
dog14230pp <43878307+dog14230pp@users.noreply.github.com>
2019-10-02 16:43:51 +0800

    Create choose.cs

---
+++ WindowsFormsApplication1/choose.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class choose : Form
+    {
+        public choose()
+        {
+            InitializeComponent();
+            button1.DialogResult = DialogResult.OK;
+        }
+
+        public static int choose_result = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.radioButton1.Checked)
+                choose_result = 0;
+            else if (this.radioButton2.Checked)
+                choose_result = 1;
+            else
+                choose_result = 2;
+
+        }
+    }
+}
